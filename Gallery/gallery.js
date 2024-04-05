0c70ecd8eeda821de49136fead70cef5f564b57b
chelozuber <108348447+chelozuber@users.noreply.github.com>
2024-04-05 08:44:29 -0300

    Commit final

---
+++ Gallery/gallery.js
@@ -0,0 +1,18 @@
+// const allImages = document.querySelectorAll(".images .img");
+// const lightbox = document.querySelector(".lightbox");
+// const closeImgBtn = lightbox.querySelector(".close-icon");
+
+// allImages.forEach(img => {
+//     img.addEventListener("click", () => showlightbox(img.querySelector("img")))
+// });
+
+// const showlightbox = (img) => {
+//     lightbox.querySelector("img").src = img;
+//     lightbox.classList.add("show");
+//     document.body.style.overflow = "hidden";
+// }
+
+// closeImgBtn.addEventListener("click", () => {
+//     lightbox.classList.remove("show");
+//     document.body.style.overflow = "auto";
+// })
